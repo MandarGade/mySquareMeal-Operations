578a2ceaf989365c990f8124226209e54a47737a
MandarGade <mandargade.2997@gmail.com>
2018-04-23 17:22:00 -0700

    'ui files updated 2'

---
+++ static/mapview.js
@@ -0,0 +1,23 @@
+fetch('../api/response.json?')
+    .then((data) => data.json())
+    .then((data) => {
+        const locations = data.map(({ restaurant_name, address: { latitude, longitude }}) => (
+            [restaurant_name, latitude, longitude]
+        ));
+        const map = new google.maps.Map(document.getElementById("map"), {
+            zoom: 10,
+            center: new google.maps.LatLng(37.328255, -121.883693),
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        });
+        const infowindow = new google.maps.InfoWindow();
+        locations.forEach(([name, lat, lng]) => {
+            const marker = new google.maps.Marker({
+                position: new google.maps.LatLng(lat, lng),
+                map: map
+            });
+            google.maps.event.addListener(marker, "click", () => {
+                infowindow.setContent(name);
+                infowindow.open(map, marker);
+            });
+        });
+    });
